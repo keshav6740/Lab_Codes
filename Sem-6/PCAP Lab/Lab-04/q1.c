2bea32e6586be486826e441ba96a72ff98f803c1
Keshav <138072394+keshav6740@users.noreply.github.com>
2025-02-01 09:48:24 +0530

    Update q1.c

--- Sem-6/PCAP Lab/Lab-04/q1.c
+++ Sem-6/PCAP Lab/Lab-04/q1.c
@@ -17,6 +17,7 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     local_fact = factorial(rank + 1);
+    printf("Process %d computed %d! = %d\n", rank, rank + 1, local_fact);
 
     MPI_Scan(&local_fact, &partial_sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
     MPI_Reduce(&partial_sum, &total_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
